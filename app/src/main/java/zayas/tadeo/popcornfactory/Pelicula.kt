6a2c69433f84bc26206d173ec72fc8737c79b81c
Tadeo-Z <fernando.zayas228310@potros.itson.edu.mx>
2024-06-12 22:46:10 -0700

    initial commit

---
+++ app/src/main/java/zayas/tadeo/popcornfactory/Pelicula.kt
@@ -0,0 +1,6 @@
+package zayas.tadeo.popcornfactory
+
+data class Pelicula(var titulo: String,
+                    var image: Int,
+                    var header: Int,
+                    var sinopsis: String)
